f657840ad2fb2fcbed68d8b0eed91bd5c4197553
CHAIPAT SUPHARAK <chaipat.sup@cpf.co.th>
2021-09-03 15:56:44 +0700

    feat: Add Excel

---
+++ src/models/excel.ts
@@ -0,0 +1,12 @@
+export interface Excel {
+    PLANT_CODE?:   number;
+    ROUND_NO?:     number;
+    ROUTE_SEQ?:    number;
+    ROUTE_CODE?:   string;
+    ROUTE_NAME?:   string;
+    DOCK_NO?:      number;
+    LOADING_TIME?: number;
+    LEAVE_TIME?:   number;
+    LATE_TIME?:    number;
+    ARRIVE_TIME?:  number;
+}
